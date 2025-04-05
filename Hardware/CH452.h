be1eee479fd2873c1d3078364877076d57710323
oihv <kokolas16@gmail.com>
2025-04-05 21:00:25 +0800

    Fix typo on Bsp spelling

--- Hardware/CH452.h
+++ Hardware/CH452.h
@@ -1,9 +1,8 @@
 #pragma once
-#include "bsp.h"
+#include "Bsp.h"
 
 void CH452_Init(void);
 void CH452_Clear(void);
-void CH452_SetDigit(u8 i,u8 n);
-void CH452_SetRam(u8 i,u8 n);
+void CH452_SetDigit(u8 i, u8 n);
+void CH452_SetRam(u8 i, u8 n);
 u8 CH452_GetKey(void);
-
