0777a9d670573185111c1e2a92d420058ba0ade9
Justin <justin.surlaya@gmail.com>
2025-04-05 22:31:14 +0800

    Added definition for backward, left, and right movement using Mecanum wheel. Motor 3 is oriented backwards so it is set opposite to general direction.

--- User/movement.c
+++ User/movement.c
@@ -4,6 +4,33 @@
 void moveForward(int rpm) {
   Set_Motor1_RPM(rpm);
   Set_Motor2_RPM(rpm);
+  Set_Motor3_RPM(-rpm);
+  Set_Motor4_RPM(rpm);
+}
+
+void moveBackward(int rpm) {
+
+  Set_Motor1_RPM(-rpm);
+  Set_Motor2_RPM(-rpm);
+  Set_Motor3_RPM(rpm);
+  Set_Motor4_RPM(-rpm);
+
+}
+
+void moveLeft(int rpm){
+
+  Set_Motor1_RPM(-rpm);
+  Set_Motor2_RPM(rpm);
+  Set_Motor3_RPM(-rpm);
+  Set_Motor4_RPM(-rpm);
+
+}
+
+void moveRight(int rpm){
+
+  Set_Motor1_RPM(rpm);
+  Set_Motor2_RPM(-rpm);
   Set_Motor3_RPM(rpm);
   Set_Motor4_RPM(rpm);
+  
 }
--- User/movement.h
+++ User/movement.h
@@ -1,3 +1,6 @@
 #include "Bsp.h"
 
 void moveForward(int rpm);
+void moveBackward(int rpm);
+void moveLeft(int rpm);
+void moveRight(int rpm);
