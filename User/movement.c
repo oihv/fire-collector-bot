c1ef846c58969c775238015bb1b5407e36426293
oihv <kokolas16@gmail.com>
2025-04-07 16:08:25 +0800

    Fix basic movement function

--- User/movement.c
+++ User/movement.c
@@ -4,31 +4,44 @@
 void moveForward(int rpm) {
   Set_Motor1_RPM(rpm);
   Set_Motor2_RPM(rpm);
-  Set_Motor3_RPM(-rpm);
+  Set_Motor3_RPM(rpm);
   Set_Motor4_RPM(rpm);
 }
 
 void moveBackward(int rpm) {
-
   Set_Motor1_RPM(-rpm);
   Set_Motor2_RPM(-rpm);
-  Set_Motor3_RPM(rpm);
-  Set_Motor4_RPM(-rpm);
-}
-
-void moveLeft(int rpm) {
-
-  Set_Motor1_RPM(-rpm);
-  Set_Motor2_RPM(rpm);
   Set_Motor3_RPM(-rpm);
   Set_Motor4_RPM(-rpm);
 }
 
-void moveRight(int rpm) {
+void moveLeft(int rpm) {
+  Set_Motor1_RPM(-rpm);
+  Set_Motor2_RPM(rpm);
+  Set_Motor3_RPM(-rpm);
+  Set_Motor4_RPM(rpm);
+}
 
+void moveRight(int rpm) {
   Set_Motor1_RPM(rpm);
   Set_Motor2_RPM(-rpm);
   Set_Motor3_RPM(rpm);
+  Set_Motor4_RPM(-rpm);
+}
+
+void rotateCW(int rpm) {
+  Set_Motor1_RPM(rpm);
+  Set_Motor2_RPM(-rpm);
+  Set_Motor3_RPM(-rpm);
   Set_Motor4_RPM(rpm);
 }
-// TODO: Rotate
+
+// Rotate counter clockwise
+void rotateCCW(int rpm) {
+  Set_Motor1_RPM(-rpm);
+  Set_Motor2_RPM(rpm);
+  Set_Motor3_RPM(rpm);
+  Set_Motor4_RPM(-rpm);
+}
+
+// TODO: implement movement based on PID output
