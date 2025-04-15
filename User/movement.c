d1e755a89bc6255c0ed7bb968f5ce9023295b5cc
ZekrBry <166268659+ZekrBry@users.noreply.github.com>
2025-04-15 21:41:54 +0700

    Fix movement

--- User/movement.c
+++ User/movement.c
@@ -26,30 +26,30 @@ void moveBackward(int rpm) {
 void moveLeft(int rpm) {
   Set_Motor1_RPM(-rpm);
   Set_Motor2_RPM(rpm);
-  Set_Motor3_RPM(-rpm);
-  Set_Motor4_RPM(rpm);
+  Set_Motor3_RPM(rpm);
+  Set_Motor4_RPM(-rpm);
 }
 
 void moveRight(int rpm) {
   Set_Motor1_RPM(rpm);
   Set_Motor2_RPM(-rpm);
-  Set_Motor3_RPM(rpm);
-  Set_Motor4_RPM(-rpm);
+  Set_Motor3_RPM(-rpm);
+  Set_Motor4_RPM(rpm);
 }
 
 void rotateCW(int rpm) {
   Set_Motor1_RPM(rpm);
-  Set_Motor2_RPM(-rpm);
+  Set_Motor2_RPM(rpm);
   Set_Motor3_RPM(-rpm);
-  Set_Motor4_RPM(rpm);
+  Set_Motor4_RPM(-rpm);
 }
 
 // Rotate counter clockwise
 void rotateCCW(int rpm) {
   Set_Motor1_RPM(-rpm);
-  Set_Motor2_RPM(rpm);
+  Set_Motor2_RPM(-rpm);
   Set_Motor3_RPM(rpm);
-  Set_Motor4_RPM(-rpm);
+  Set_Motor4_RPM(rpm);
 }
 
 void stopMotor() {
